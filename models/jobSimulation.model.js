d4d983e853b092b8616973f81e2403c0f5825d1e
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-20 17:33:59 +0000

    feat: added jobSimulation schema

---
+++ models/jobSimulation.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const jobSimulationSchema = new mongoose.Schema({
+  imageUrl:{type:String},
+  imagePublicId:{type:String},
+  title: { type: String, required: true },
+  description: { type: String },
+  field: { type: String, enum:['IT','Marketing','Law','Software Development','Database Design','Accounting','Development Studies','Education','Statistics','Business Administration'] },
+  companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+  companyName: { type: String }, // denormalized 
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tasks' }],
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  level: { type: String, enum: ['Beginner', 'Intermediate', 'Advanced'] },
+  duration: { type: Number }, // in hours
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  isHiring:{type:Boolean},
+  isPublished: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model("jobSimulation",jobSimulationSchema);
