6a2318dc172d4721d862c524ba42c2a769991b24
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-20 23:13:43 +0000

    feat: added swagger keywords to allow file uploads

--- routes/recruiterRoute.route.js
+++ routes/recruiterRoute.route.js
@@ -15,9 +15,46 @@ authorizeRole("recruiter"),companyController.createCompany);
 router.put('/edit-company/:id',uploadImage.single("file"),authMiddleware,authorizeRole("recruiter"),companyController.editCompany);
 
 //JOB SIM ROUTES
+
+// File: routes/recruiterRoute.route.js
+
+/**
+ * @swagger
+ * /upload:
+ *   post:
+ *     summary: Upload a file
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: file
+ *         type: file
+ *         required: true
+ *         description: The file to upload
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ */
 router.post('/create-job-sim',uploadImage.single("file"),
 authMiddleware,authorizeRole("recruiter"),jobSimulationController.createJobSim);
 
+/**
+ * @swagger
+ * /upload:
+ *   post:
+ *     summary: Upload a file
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: file
+ *         type: file
+ *         required: true
+ *         description: The file to upload
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ */
 router.put('/edit-job-sim/:id',uploadImage.single("file"),
 authMiddleware,authorizeRole("recruiter"),jobSimulationController.editJobSimById);
 
