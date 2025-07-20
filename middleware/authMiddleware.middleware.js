71aa2902a1bcd5910f961a5e91b6c3a3a4e2d297
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-20 09:58:11 +0000

    feat: added authentication and authorization middleware

---
+++ middleware/authMiddleware.middleware.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const { errorMessage } = require("../utils/responseHandler.util");
+const User= require("../models/userModel.model");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const authMiddleware= (req,res,next) =>{
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(401).json({
+            message: "You need to login!"
+        });
+    }
+    console.log("AuthHeader",authHeader)
+
+    const token = authHeader.split(" ")[1];
+
+    console.log("token",token);
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+}
+
+const allowedRoles = ["student","recruiter","admin"];
+
+function authorizeRole(...allowedRoles){
+    return async (req,res,next) =>{
+        if(!allowedRoles.includes(req.user.role)){
+            return errorMessage(res,403,"Access Forbidden")
+        }
+        const user= await User.findById(req.user.id);
+        console.log("user=",user)
+        if (user.role ==="recruiter"){
+            if(!user.isVerified){
+               return errorMessage(res,403,"You must be verified to proceed");
+            }
+        }
+        next();
+    }
+}
+
+module.exports={ 
+    authorizeRole,
+    authMiddleware,
+    allowedRoles
+}
