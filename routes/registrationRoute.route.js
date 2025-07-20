cd43d26e16798cec60e82e80d0bb4ca4767fb795
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-20 23:16:07 +0000

    feat: added swagger keywords to allow file uploads,added multer middleware fpr file uploads

--- routes/registrationRoute.route.js
+++ routes/registrationRoute.route.js
@@ -1,9 +1,28 @@
 const {Router} = require("express");
 const { signup, login } = require("../controllers/authController.controller");
+const { uploadImage } = require("../config/fileUpload.config");
 const router = Router();
 
 
-router.post("/signup",signup);
+// File: routes/regsitrationRoute.route.js
+/**
+ * @swagger
+ * /upload:
+ *   post:
+ *     summary: Upload a file
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: file
+ *         type: file
+ *         required: true
+ *         description: The file to upload
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ */
+router.post("/signup",uploadImage.single("file"),signup);
 router.post("/login",login);
 
 module.exports= router;
