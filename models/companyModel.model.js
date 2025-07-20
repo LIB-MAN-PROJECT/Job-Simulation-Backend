1555d9c35409beecdb9a65a1fe014898d44d122d
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-20 17:31:36 +0000

    feat: added Company Model

---
+++ models/companyModel.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//only for recruiters
+const companySchema = new mongoose.Schema({
+  verifiedId:{type:String},
+  companyName: { type: String, required: true },
+  companyCode: {type:String},
+  companyEmail:{type:String,unique:true},
+  description: { type: String },
+  logoUrl: { type: String },
+  logoPublicId:{ type: String},
+  website: { type: String },
+  recruiters: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  isVerified: {type:Boolean, default:false} //not needed in the frontend
+}, { timestamps: true });
+
+module.exports = mongoose.model("company",companySchema);
