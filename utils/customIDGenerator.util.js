884b146e3a6ccfee8f134e2ebc3dab0f1f1bbcde
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-21 23:50:04 +0000

    feat: add code to generate IDs

---
+++ utils/customIDGenerator.util.js
@@ -0,0 +1,18 @@
+const {customAlphabet} = require("nanoid");
+const Company = require("../models/companyModel.model");
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',7);
+
+// const generateCustomID = () =>{
+//     const customID = nanoid(7);
+//     return customID;
+// }
+
+const hybridCompanyID= (prefix='CM') =>{
+    const year =new Date().getFullYear().toString().slice(-2);
+    const uniqueCode =nanoid();
+    const customID= `${prefix}-${year}-${uniqueCode}`;
+    return customID;
+}
+
+module.exports = hybridCompanyID
