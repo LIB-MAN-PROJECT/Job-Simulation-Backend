3cc18a5b4a6480d6ff901fb1c6b4a5f9dc0a3b15
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-21 23:47:07 +0000

    fix: refactored code

--- routes/userRoute.route.js
+++ routes/userRoute.route.js
@@ -4,21 +4,22 @@ const jobSimulationController= require("../controllers/jobSimulationController.c
 const enrollmentController = require("../controllers/enrollmentController.controller");
 const { authMiddleware, authorizeRole } = require("../middleware/authMiddleware.middleware");
 const taskSubmissionController =require("../controllers/taskSubmissionController.controller");
+const reviewController= require("../controllers/reviewController");
 const { uploadDocument } = require("../config/fileUpload.config");
 const router= Router();
 
 //SIMULATIONS
 router.get('/view-all-simulations',jobSimulationController.viewAllJobSims)
 
-router.get('/view-simulations/:id',jobSimulationController.viewJobSimsById);
+router.get('/view-simulations/:simulationId',jobSimulationController.viewJobSimsById);
 
 router.get('/simulations/search',jobSimulationController.searchSimulations);
 
 //ENROLLMENT
-router.post('/simulation/:id/enroll',authMiddleware,authorizeRole("student"),enrollmentController.enrollInJobSim);
+router.post('/simulation/:simulationId/enroll',authMiddleware,authorizeRole("student"),enrollmentController.enrollInJobSim);
 
 //Unenroll
-//  router.delete('/simulation/:userid/simulations/:simid',enrollmentController.unenrollInJobSim);
+//  router.delete('/simulation/:userid/simulations/:simulationId',enrollmentController.unenrollInJobSim);
 
 //Submitting tasks
 router.post('/simulation/:simulationId/task/:taskId/submit-task',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),taskSubmissionController.submitTask);
@@ -26,15 +27,19 @@ router.post('/simulation/:simulationId/task/:taskId/submit-task',authMiddleware,
 //Editing Submitted task
 router.put('/simulation/:simulationId/task/:taskId/submission/:taskSubmissonId',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),taskSubmissionController.editSubmittedTask);
 
+//REVIEWS
+router.post('/simulation/:simulationId/create-review',authMiddleware,authorizeRole("student"),reviewController.sendReview);
+
+router.put('/simulation/:simulationId/:reviewId/edit-review',authMiddleware,authorizeRole("student"),reviewController.editReview);
+
+
 //INTERNSHIP POSTS
 router.get('/view-all-internships',internshipController.viewAllInternshipPosts);
 
-router.get('/view-internships/:id',internshipController.viewInternshipPostById);
+router.get('/view-internships/:internshipId',internshipController.viewInternshipPostById);
 
 router.post('/view-internships/:internshipId/apply',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),internshipController.applyForInternship);
 
 router.get('/internships/search',internshipController.searchInternships);
-
-
 
 module.exports = router;
