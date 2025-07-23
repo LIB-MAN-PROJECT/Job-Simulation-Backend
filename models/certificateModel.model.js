d4fb71187e7cbbfe5895b9a0f0668181bbb41c84
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-23 15:01:39 +0000

    fix: changed model alias

--- models/certificateModel.model.js
+++ models/certificateModel.model.js
@@ -4,9 +4,10 @@ const certificateSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     fullName: { type: String },
     simulationId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobSimulation', required: true },
+    enrollmentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Enrollment', required: true },
     certUrl: { type: String, required: true },
-    certId: { type: String, required: true, unique: true },
+    certPublicId: { type: String, required: true, unique: true },
     issuedAt: { type: Date, default: Date.now }
 }, { timestamps: true });
 
-module.exports = mongoose.model("certificate", certificateSchema);
+module.exports = mongoose.model("Certificate", certificateSchema);
