dc30bdb1445e11edc24889e962ac66b1314b71ab
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-23 15:07:24 +0000

    fix: changed reviewState and review to feedbackState and feedback

--- models/enrollmentModel.model.js
+++ models/enrollmentModel.model.js
@@ -6,14 +6,17 @@ const enrollmentSchema = new mongoose.Schema({
   lastName:{type:String},
   simulationId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobSimulation', required: true },
   progress: { type: Number, default: 0 },
-  enrolledAt: { type: Date, default: Date.now },
+  taskSubmissions:[{type:mongoose.Schema.Types.ObjectId,ref:'TaskSubmission'}],
+  enrolledAt: { type: Date},
   completedAt: { type: Date },
   isReadyForReview:{type:Boolean},
   isReviewed:{type:Boolean},
-  reviewState:{type:String, enum:['Accepted','Pending ','Rejected']},
+  feedbackState:{type:String, enum:["Accepted",'Pending','Rejected']},
+  reviewedAt: Date,
+  feedback: String
 }, { timestamps: true });
 
 // preventing duplicate enrollments
 enrollmentSchema.index({ userId: 1, simulationId: 1 }, { unique: true });
 
-module.exports= mongoose.model("enrollment",enrollmentSchema);
+module.exports= mongoose.model("Enrollment",enrollmentSchema);
