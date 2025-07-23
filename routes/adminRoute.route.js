810fb55255ed565a8d8a9c67f075c20b3ee48773
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-23 22:11:30 +0000

    feat: added dashboard routes

--- routes/adminRoute.route.js
+++ routes/adminRoute.route.js
@@ -1,11 +1,18 @@
 const {Router} = require("express");
 const router = Router();
 const {verifyRecruiter,unverifyRecruiter} = require("../controllers/adminController.controller");
-
+const adminController= require("../controllers/adminController.controller");
+const { authMiddleware, authorizeRole } = require("../middleware/authMiddleware.middleware");
 
 // router.post('/create-company',createCompany);
 
-router.patch('/verify-recruiter/:id',verifyRecruiter);
-router.patch('/unverify-recruiter/:id',unverifyRecruiter);
+router.get('/profile/overview',authMiddleware,authorizeRole("admin"),adminController.getAppAnalytics);
+router.get('/profile/users/students',authMiddleware,authorizeRole("admin"),adminController.getStudents);
+router.get('/profile/users/recruiters',authMiddleware,authorizeRole("admin"),adminController.getRecruiters);
+router.get('/profile/companies',authMiddleware,authorizeRole("admin"),adminController.getCompanies);
+
+
+router.patch('/profile/verify-recruiter/:id',verifyRecruiter);
+router.patch('/profile/unverify-recruiter/:id',unverifyRecruiter);
 
 module.exports=router;
