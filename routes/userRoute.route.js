0215bd79e942ff3aa2fc674f0170bbe69d2f5d1e
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-24 09:31:27 +0000

    feat: added code for protected route docs

--- routes/userRoute.route.js
+++ routes/userRoute.route.js
@@ -12,27 +12,35 @@ const router= Router();
 //SIMULATIONS
 router.get('/view-all-simulations',jobSimulationController.viewAllJobSims)
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/view-simulations/:simulationId',authMiddleware,jobSimulationController.viewJobSimsById);
 
 router.get('/simulations/search',jobSimulationController.searchSimulations);
 
 //ENROLLMENT
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.post('/simulation/:simulationId/enroll',authMiddleware,authorizeRole("student"),enrollmentController.enrollInJobSim);
 
 //Unenroll
-// router.delete('/profile/simulation/:userid/simulations/:simulationId',enrollmentController.unenrollInJobSim);
+
+router.delete('/profile/simulation/:userid/simulations/:simulationId',enrollmentController.unenrollInJobSim);
 
 //Submitting tasks
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.post('/simulation/:simulationId/task/:taskId/submit-task',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),taskSubmissionController.submitTask);
 
 //Editing Submitted task
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.put('/simulation/:simulationId/task/:taskId/submission/:taskSubmissonId/edit-submission',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),taskSubmissionController.editSubmittedTask);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.delete('/simulation/:simulationId/task/:taskId/submission/:taskSubmissonId/delete-submission',authMiddleware,authorizeRole("student"),taskSubmissionController.deleteSubmittedTask);
 
 //REVIEWS
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.post('/simulation/:simulationId/create-review',authMiddleware,authorizeRole("student"),reviewController.sendReview);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.put('/simulation/:simulationId/:reviewId/edit-review',authMiddleware,authorizeRole("student"),reviewController.editReview);
 
 
@@ -41,6 +49,7 @@ router.get('/view-all-internships',internshipController.viewAllInternshipPosts);
 
 router.get('/view-internships/:internshipId',internshipController.viewInternshipPostById);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.post('/view-internships/:internshipId/apply',authMiddleware,authorizeRole("student"),uploadDocument.single("file"),internshipController.applyForInternship);
 
 router.get('/internships/search',internshipController.searchInternships);
@@ -48,14 +57,19 @@ router.get('/internships/search',internshipController.searchInternships);
 
 //USER PROFILE
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/profile/stats',authMiddleware,authorizeRole("student"),userProfileController.getUserStats);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/profile/account',authMiddleware,authorizeRole("student"),userProfileController.getUserProfile);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/profile/enrollments',authMiddleware,authorizeRole("student"),userProfileController.getEnrolledSimulations);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/profile/applications',authMiddleware,authorizeRole("student"),userProfileController.getAppliedInternships);
 
+  // #swagger.security = [{ "bearerAuth": [] }]
 router.get('/profile/certificates',authMiddleware,authorizeRole("student"),userProfileController.getCertificates);
 
 module.exports = router;
