e976a21197147cf95344f1c18655f00a262906ef
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-07-28 11:52:26 +0000

    feat: added spacing in lines

--- utils/uniqueCustomIdCheck.util.js
+++ utils/uniqueCustomIdCheck.util.js
@@ -2,21 +2,21 @@ const Company = require("../models/companyModel.model");
 const hybridCompanyID = require("./customIDGenerator.util");
 
 
-const uniqueCompanyId = async()=>{
+const uniqueCompanyId = async () => {
     let companyCustomId;
-    let exists =true;
+    let exists = true;
     let attempts = 0;
     const MAX_ATTEMPTS = 5;
     //keep generating until an unused ID is found
 
-    do{
+    do {
         companyCustomId = hybridCompanyID();
-        exists= await Company.exists({companyCustomId});
+        exists = await Company.exists({ companyCustomId });
         attempts++;
-        if (attempts > MAX_ATTEMPTS){
+        if (attempts > MAX_ATTEMPTS) {
             throw new Error("Too many ID collisions. Try again or increase ID length.")
         }
-    }while(exists)
+    } while (exists)
 
     return companyCustomId;
 }
