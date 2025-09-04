f8feb8b9c003c1da93722c6861fc3a094f7e77dc
Nana Ebow <nana.enyan-aidoo@st.vvu.edu.gh>
2025-09-04 16:22:31 +0000

    feat: added connection host as logged message

--- config/db.config.js
+++ config/db.config.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
+        const connection= await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
         console.log(error.message);
         process.exit(1)
