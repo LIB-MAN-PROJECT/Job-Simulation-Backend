6362b7d529308bcf860961694d2ca6dba69ca8ae
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2025-10-10 01:00:11 +0000

    feat: added newsignup route

--- routes/registrationRoute.route.js
+++ routes/registrationRoute.route.js
@@ -1,5 +1,5 @@
 const {Router} = require("express");
-const { signup, login } = require("../controllers/authController.controller");
+const { signup, login,newSignUp } = require("../controllers/authController.controller");
 const { uploadImage } = require("../config/fileUpload.config");
 const validateSchema = require("../middleware/validateRequest.middleware");
 const signupSchema = require("../validation/signupSchema.schema");
@@ -29,6 +29,12 @@ router.post("/signup",uploadImage.single("file"),validateSchema(signupSchema),
 // #swagger.tags=['Users','Recruiters', 'Admins']
 // #swagger.summary = 'Sign Up'
 signup);
+
+router.post("/newsignup",validateSchema(signupSchema),
+// #swagger.tags = ['Users','Recruiters','Admins']
+// #swagger.summary = 'New Sign Up (Untested)'
+newSignUp);
+
 router.post("/login",validateSchema(loginSchema),
 // #swagger.tags=['Users','Recruiters', 'Admins']
 // #swagger.summary = 'Log In'
